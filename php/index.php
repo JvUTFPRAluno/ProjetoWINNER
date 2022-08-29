93913778b8d033f0edd7b3bdec7ddbdfa97c773d
JvDev2003 <jvsp20032003@gmail.com>
2022-08-29 17:28:45 -0300

    Delete index.php

--- php/index.php
+++
@@ -1,9 +0,0 @@
-<?php include'parts/header.php'; ?>
-
-    <div class="conteiner-banner">
-        <div class="overlay">
-
-        </div><!--overlay-->
-    </div><!--conteiner-banner-->
-
-<?php include'parts/footer.php'; ?>
