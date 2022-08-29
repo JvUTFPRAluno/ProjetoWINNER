470891cd2c3f9cd990efa84d8c71d3d215ccd50b
JvUTFPRAluno <111456239+JvUTFPRAluno@users.noreply.github.com>
2022-08-29 08:12:37 -0300

    comentarios
    adicionado comentários para melhor entendimento do codigo

--- php/config.php
+++ php/config.php
@@ -1,9 +1,11 @@
 <?php
+	//procedimento que faz a conexão com banco de dados.
 
-	$servername ="localhost";
-	$database ="winner";
-	$username ="root";
-	$senha = "";
-
+	$servername ="localhost";//nome do server
+	$database ="winner";//nome do banco de dados
+	$username ="root";//nome do usuario
+	$senha = "";//senha do usuario
+	
+	//tenta fazer a conexão caso falhe retorna um erro de conexão.
 	$conn = mysqli_connect($servername,$username,$senha,$database) or die('ERRO!!' . mysqli_connect_error());
 ?>
