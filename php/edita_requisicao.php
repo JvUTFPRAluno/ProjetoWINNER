ea4d82cb1e37b55d4a090f5b6ab30394bdea3c02
JvUTFPRAluno <111456239+JvUTFPRAluno@users.noreply.github.com>
2022-08-29 08:26:58 -0300

    comentarios
    adicionado comentários para melhor entendimento do código

--- php/edita_requisicao.php
+++ php/edita_requisicao.php
@@ -1,7 +1,8 @@
 <?php
-    include 'config.php';
+    include 'config.php';//inicia o banco de dados
 
-	$id = $_POST['id'];
+    //pega e atribui as variaveis no metodo POST
+    $id = $_POST['id'];
     $tipo = implode(",",$_POST['tipo_manutencao']);
     $terceiros = $_POST['terceiros'];
     $nome = $_POST['nome_funcionario'];
@@ -9,12 +10,13 @@
     $motivo = $_POST['motivo_falha'];
     $parada = $_POST['tempo_parada'];
     $solucao = $_POST['solucao'];
-
+    
+    //query que atualiza os dados mudados pelo usuario no editar.php
     $sql = "UPDATE `ordemservico` SET `tipoManutencao`='$tipo',`nomeManutencao`='$nome',`parteProblema`='$parte',`motivoProblema`='$motivo',`solucao`='$solucao',
     `tempo_parada`='$parada',`nomeTerceiros`='$terceros',`concluido`='1' WHERE `id`='$id'";
-    mysqli_query($conn, $sql);
+    mysqli_query($conn, $sql);//executa a query
 
-    mysqli_close($conn);
-    header('Location: '. '../ordens.php');
-    die();
+    mysqli_close($conn);// fecha o banco de dados
+    header('Location: '. '../ordens.php'); // redireciona para ordens
+    die();//mata o procedimento
 ?>
