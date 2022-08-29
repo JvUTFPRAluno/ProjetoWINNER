7ef1d1f7bfd0a5552ee40e222cad07bf14cc3ea1
JvUTFPRAluno <111456239+JvUTFPRAluno@users.noreply.github.com>
2022-08-29 08:58:30 -0300

    comentarios
    adicionado comentários para melhor entendimento do código

--- php/mostra_dados_req.php
+++ php/mostra_dados_req.php
@@ -1,15 +1,18 @@
 <?php
-    include 'config.php';
+    include 'config.php';//inicia o banco de dados
 
-	$id = $_GET['id'];
-	$tempoInicio = date('Y-m-d H:i');
+    $id = $_GET['id'];// pega o id da requisição a ser resolvida
+    $tempoInicio = date('Y-m-d H:i'); // atribui o tempo e a data atual 
+	
+    //query que atribui o valor da variavel '$tempoInicio' a coluna dataInicio
+    $sql2 = "UPDATE ordemservico SET `dataInicio` = '$tempoInicio' WHERE `id` = '$id'";
+    mysqli_query($conn, $sql2);//executa query
 
-	$sql2 = "UPDATE ordemservico SET `dataInicio` = '$tempoInicio' WHERE `id` = '$id'";
-	mysqli_query($conn, $sql2);
+    $sql = "SELECT * FROM `ordemservico` WHERE id = $id LIMIT 1";//query que pega os dados onde a coluna id = '$id'
+    $result = mysqli_query($conn, $sql);//executa a query e adiciona o resultado a variavel '$result'  
+    $linha = mysqli_fetch_array($result);//adiciona os dados de '$result' em '$linha' como uma array
 
-    $sql = "SELECT * FROM `ordemservico` WHERE id = $id LIMIT 1";
-    $result = mysqli_query($conn, $sql);
-    $linha = mysqli_fetch_array($result);
+    // atribui esse valor a um formulario que não pode ser enviado (serve apenas para consulta)
 ?>
 	<section class="requisitante">
 		<form>
