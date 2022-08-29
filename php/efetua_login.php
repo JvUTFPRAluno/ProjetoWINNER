9d48223deebf1fb813ad61d77178593b678f2c70
JvUTFPRAluno <111456239+JvUTFPRAluno@users.noreply.github.com>
2022-08-29 08:36:11 -0300

    comentarios
    adicionado comentários para melhor entendimento do código

--- php/efetua_login.php
+++ php/efetua_login.php
@@ -1,30 +1,36 @@
 <?php
-    if (!empty($_POST) AND (empty($_POST['login_codigo']) OR empty($_POST['login_senha']))) {
-        header("Location: ../index.php"); exit;
+    //procedimento que efetua o inicio de sessão a partir do login correto do usuario 
+    if (!empty($_POST) AND (empty($_POST['login_codigo']) OR empty($_POST['login_senha']))) {//verifica se não existe algum campom vazio
+        header("Location: ../index.php"); exit;// se houver redirecionado o usuario para o index
     }
 
-    include 'config.php';
+    include 'config.php';//inicia o banco de dados
     
+    //pega as variaveis e as atribui pelo metodo POST
     $codigo = $_POST['login_codigo'];
     $senha = $_POST['login_senha'];
 
+    //query que procura no banco de dado, alguma correspondencia com os dados do login
     $sql = "SELECT * FROM usuarios WHERE codigo = '$codigo' AND senha = '$senha' LIMIT 1";
-    $result = mysqli_query($conn,$sql);
+    $result = mysqli_query($conn,$sql);//executa a query
 
-    if(mysqli_num_rows($result) != 1){
-        echo('<script>alert("Codigo e/ou Senha incorretos!!!");</script>');
-        header("Location: ../login.php");
-        exit;
+    if(mysqli_num_rows($result) != 1){//verifica se existe correspondencia('se existir nenhuma ou mais de uma retorna um erro e redireciona para o login')
+        
+        echo('<script>alert("Codigo e/ou Senha incorretos!!!");</script>');//exibe um alerta sobre o erro no login
+        header("Location: ../login.php");//redireciona para o login
+        exit;//mata o codigo 
+    
     }
 
-    $resultado = mysqli_fetch_array($result);
+    $resultado = mysqli_fetch_array($result);//se as condições se cumprirem adiciona os dados a variavel '$resultado'
 
-    if (!isset($_SESSION)) session_start();
+    if (!isset($_SESSION)) session_start();//inicia a sessão
 
+    //atribui os dados a sessão
     $_SESSION['USUARIOCOD'] = $resultado['codigo'];
     $_SESSION['USUARIOSENHA'] = $resultado['senha'];
     $_SESSION['USUARIOPREV'] = $resultado['previlegio'];
 
-    header("Location: ../ordens.php");
-    exit;
+    header("Location: ../ordens.php");//redireciona para ordens 
+    exit;//mata o codigo
 ?>
