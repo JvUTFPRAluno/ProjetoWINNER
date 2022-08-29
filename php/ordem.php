e3746c407287b9243b67d6a1d77b33fa6eb16547
JvDev2003 <jvsp20032003@gmail.com>
2022-08-29 17:28:37 -0300

    Delete ordem.php

--- php/ordem.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include 'parts/header.php';//adiciona o cabeçalho da pagina
-    include 'php/mostra_dados_req.php';
-    include 'php/mostrarTudo.php';//mostra dados a serem editados
-    include 'parts/footer.php';//adiciona o rodapé da pagina
-?>
