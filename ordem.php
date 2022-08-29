15bfee542a55bca733631295c773c7257f1710c6
JvUTFPRAluno <111456239+JvUTFPRAluno@users.noreply.github.com>
2022-08-29 09:18:41 -0300

    comentarios
    adicionado comentários para melhor entendimento do código

--- ordem.php
+++ ordem.php
@@ -1,6 +1,6 @@
 <?php
-    include 'parts/header.php';
+    include 'parts/header.php';//adiciona o cabeçalho da pagina
     include 'php/mostra_dados_req.php';
-    include 'php/mostrarTudo.php';
-    include 'parts/footer.php';
+    include 'php/mostrarTudo.php';//mostra dados a serem editados
+    include 'parts/footer.php';//adiciona o rodapé da pagina
 ?>
