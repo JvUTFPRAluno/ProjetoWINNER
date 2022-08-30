a62ae0fd11f48e85177266b4de16b7a08f91f0fc
JvDev2003 <jvsp20032003@gmail.com>
2022-08-30 20:52:16 -0300

    Update editar.php

--- editar.php
+++ editar.php
@@ -1,6 +1,11 @@
 <?php include 'parts/header.php';?>
 
 <?php 
+
+    require_once 'php/verifica_sessao.php';
+
+    verifica_conectado(1);
+
     include 'php/mostra_dados_req.php';
 
     include 'php/mostra_dados_manutencao.php';
