33512e70b9e034100afa335f9239f564514204c2
JvDev2003 <jvsp20032003@gmail.com>
2022-09-07 17:24:59 -0300

    botão para imprimir
    adicionado botão que possibilita imprimir dados

--- ordem.php
+++ ordem.php
@@ -2,5 +2,10 @@
     include 'parts/header.php';//adiciona o cabeçalho da pagina
     include 'php/mostra_dados_req.php';
     include 'php/mostrarTudo.php';//mostra dados a serem editados
+?>
+    <div class="imprimir_req">
+        <a href="imprimirReq.php?id=<?php echo $_GET['id']?>">IMPRIMIR</a>
+    </div>
+<?php
     include 'parts/footer.php';//adiciona o rodapé da pagina
 ?>
