e03979da67f23b5abdfabc46d810ae524d1e058f
JvDev2003 <jvsp20032003@gmail.com>
2022-09-07 17:21:36 -0300

    Validações
    feito validações dos formulários

--- js/func.js
+++ js/func.js
@@ -1,14 +1,69 @@
 $(function(){
-	function alerta (campo){
-		alert("O campo ",campo," precisa ser preenchido!!!");
+	//executa as funções
+	apareceElemento('#sim_t', '#campoTerceiros');
+	escondeElemento('#nao_t', '#campoTerceiros');
+
+	apareceElemento('#sim_p', '#tempo_parada');
+	escondeElemento('#nao_p', '#tempo_parada');
+
+	function alerta (campo){//da um alerta caso o campo esteja vazio
+		alert("O campo " + campo +" precisa ser preenchido!!!");
 	}
 
+	function apareceElemento(elemento, aparece){//faz o elemento aparecer
+		$(elemento).click(()=>{
+			$(aparece).show();
+		})
+	}
 
-    $('form').submit(()=>{
-		if($('#sim_t').is(":checked")){
-			alerta('terceiros');
+	function escondeElemento(elemento, esconde){//faz o elemento sumir
+		$(elemento).click(()=>{
+			$(esconde).hide();
+		})
+
+		if($(elemento).is(":checked")){
+			$(esconde).hide()
 		}
-		return false;
+	}
+
+	function apareceLinha(selectprimario, afetado){
+		if($(selectprimario).val() == 'Produção'){
+			$(afetado).show();
+		}else{
+			$(afetado).hide();
+		}
+	}
+
+	$('#filtro').click(()=>{//faz o elemento aparecer somente se o usuario for da produção
+		apareceLinha('#setorFiltro','#linhaProd');
+	})
+
+	$('#requisitanteForm').click(()=>{//faz o elemento aparecer somente se o usuario for da produção
+		apareceLinha('#setorReq','#linhaReq');
+	})
+
+
+    $('#relatorio').submit(()=>{		
+		if($('#sim_t').is(":checked")){
+			if($('#terceiros').val() == ""){
+				alerta('terceiros');
+				return false;
+			}
+		}
+
+		if($('#sim_p').is(":checked")){
+			if($('#tempo_parada').val() == "00:00"){
+				alerta('tempo de parada');
+				return false;
+			}
+		}
+
+		if($('#tManutencao').val() == ""){
+				alerta('tipo de manutenção');
+				return false;
+		}
+
+		return true;
 	})
 });
     
