d4c75c48b43f41d31e57a64fdb939a657742b215
JvDev2003 <jvsp20032003@gmail.com>
2022-09-07 17:20:46 -0300

    Jquery
    alterado método de chamada do jquery por conta de bug

--- parts/header.php
+++ parts/header.php
@@ -7,7 +7,7 @@
 	<title>usuario</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-	<script scr="js/jquery.js"></script>
+	<script src="js/jquery.js" type="text/javascript"></script>
 	<script src="js/func.js"></script>
 </head>
 <body>
