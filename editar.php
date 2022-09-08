218d8a24b5dd82ff7835b5e8a4c76a00eca4e325
JvDev2003 <jvsp20032003@gmail.com>
2022-09-07 22:54:22 -0300

    mecanismo de erro atualizado
    retirado mecanismo antigo

--- editar.php
+++ editar.php
@@ -1,7 +1,6 @@
 <?php include 'parts/header.php';?>
 
 <?php 
-
     require_once 'php/verifica_sessao.php';
 
     verifica_conectado(1);
@@ -9,6 +8,9 @@
     include 'php/mostra_dados_req.php';
 
     include 'php/mostra_dados_manutencao.php';
+
 ?>
 
-<?php include 'parts/footer.php';?>
+<?php
+    include 'parts/footer.php';
+?>
