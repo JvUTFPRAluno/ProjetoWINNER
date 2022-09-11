07a103d66013517385a849448f94c17749c159f5
JvDev2003 <jvsp20032003@gmail.com>
2022-09-11 11:38:51 -0300

    titulo
    consertado titulo do site

--- parts/header.php
+++ parts/header.php
@@ -4,13 +4,14 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>usuario</title>
+	<title>Sistema de OS</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	<script src="js/jquery.js" type="text/javascript"></script>
 	<script src="js/func.js"></script>
 </head>
 <body>
+	
 	<header>
 		<h1>AP Winner</h1>
 		<div class="top-menu">
@@ -18,7 +19,7 @@
 			<div class="item"><a href="Usuario.php">Usuario</a></div>
 			<div class="item"><a href="ordens.php">Ordens</a></div>
 			<div class="item"><a href="ordensConcluidas.php">Concluidas</a></div>
-			<div class="item last bottom_line"><a href="php/logout.php">Sair</a></div>
+			<div class="item last"><a href="php/logout.php">Sair</a></div>
 			<div class="clear"></div>
 		</div><!--top-menu-->	
 	</header>
