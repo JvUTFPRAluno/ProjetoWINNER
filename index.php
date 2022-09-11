49710ad87f72e824e1cc3375c90bc5458852a766
JvDev2003 <jvsp20032003@gmail.com>
2022-09-11 11:37:54 -0300

    consertado
    adicionado conteúdo ao index

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 
     <div class="conteiner-banner">
         <div class="overlay">
-
+            <div>
+                <h2>Sistema de ordem de serviços</h2>
+                <p>Bem-vindo ao sistema de ordem e serviço da AP Winner</p>
+            </div>
         </div><!--overlay-->
     </div><!--conteiner-banner-->
 
