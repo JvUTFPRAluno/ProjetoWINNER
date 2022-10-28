f19a747392a2e28b3dd1f87e08eccdf17e200797
JvUTFPRAluno <111456239+JvUTFPRAluno@users.noreply.github.com>
2022-10-28 06:16:54 -0300

    Add files via upload

--- winner.sql
+++ winner.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11-Set-2022 às 16:44
+-- Tempo de geração: 28-Out-2022 às 11:14
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -33,41 +33,50 @@ CREATE TABLE `ordemservico` (
   `setor` varchar(20) NOT NULL,
   `linhaProducao` varchar(40) DEFAULT NULL,
   `dataRequisicao` datetime NOT NULL,
-  `tipoManutencao` varchar(20) NOT NULL,
-  `nomeManutencao` varchar(50) NOT NULL,
-  `dataInicio` datetime NOT NULL,
-  `dataTermino` datetime NOT NULL,
-  `parteProblema` varchar(50) NOT NULL,
-  `motivoProblema` text NOT NULL,
   `descricaoRequisicao` text NOT NULL,
-  `solucao` text NOT NULL,
-  `tempo_parada` time NOT NULL,
-  `nomeTerceiros` varchar(30) DEFAULT NULL,
-  `concluido` int(1) NOT NULL DEFAULT 0
+  `concluido` int(1) NOT NULL DEFAULT 0,
+  `id_relatorioTecnico` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `ordemservico`
 --
 
-INSERT INTO `ordemservico` (`id`, `nomeRequisitante`, `setor`, `linhaProducao`, `dataRequisicao`, `tipoManutencao`, `nomeManutencao`, `dataInicio`, `dataTermino`, `parteProblema`, `motivoProblema`, `descricaoRequisicao`, `solucao`, `tempo_parada`, `nomeTerceiros`, `concluido`) VALUES
-(2, 'joana', 'Produção', '12-Terco 1', '2022-05-11 16:28:45', 'C. emergencial', 'rafael', '2022-09-08 15:56:00', '2022-09-08 19:32:00', 'rotuladeria', '', 'algo aconteceu novamente', 'parafuso apertado', '00:15:00', '', 1),
-(5, 'Diego', 'Produção', '01-Arla antiga', '2022-08-23 18:45:00', 'C. emergencial', 'cleber', '2022-09-07 16:09:00', '2022-09-07 17:05:00', 'chacoalhadeira', 'vazamento', 'uma maquina quebrou', 'apertado junta', '00:00:00', '', 1),
-(7, 'joana', 'Produção', '01-Arla antiga', '2022-07-23 23:14:00', 'Ajuste operacional', 'rafael', '2022-09-06 19:43:00', '2022-09-07 20:15:00', 'chacoalhadeira', 'parafuso solto', 'algo ocorreu', 'apertado parafuso', '00:00:00', '', 1),
-(8, 'pedro', 'Produção', '01-Arla antiga', '2022-08-24 04:15:00', 'Melhoria', 'rafael', '2022-09-06 19:00:00', '2022-08-24 04:16:00', 'chacoalhadeira', 'parafuso solto', 'algum problema', 'parafuso apertado', '00:00:00', 'null', 1),
-(9, 'Joao santos', 'Produção', '01-Arla antiga', '2022-08-27 01:27:00', 'C. emergencial', 'Luis', '2022-09-07 12:25:00', '2022-09-06 18:52:00', 'chacoalhadeira', 'vazamento', 'alguma coisa esta vazando', 'apertado cano', '00:00:00', '', 1),
-(10, 'joana', 'Produção', '12-Terco 1', '2022-08-29 15:48:00', 'C. emergencial', 'cleber', '2022-09-06 19:12:00', '2022-09-06 19:12:00', 'rotuladeria', 'parafuso solto', 'algo esta vazando', 'algo ocorreu', '00:00:00', '', 1),
-(12, 'joao santos', 'Administrativo', '', '2022-09-03 17:27:00', 'Predial', 'Luis', '2022-09-06 15:27:00', '2022-09-07 17:30:00', 'Cadeira', 'quebrou a perna', 'quebrou a cadeira', 'soldado perna', '00:00:00', '', 1),
-(16, 'joao santos', 'Administrativo', '', '2022-09-07 13:01:00', 'C. emergencial', 'rafael', '2022-09-07 15:30:00', '2022-09-07 18:00:00', 'algo', 'quebrou', 'algo quebrou', 'algo foi consertado', '00:00:00', '', 1),
-(17, 'joana', 'Administrativo', '', '2022-09-07 14:59:00', 'C. emergencial', 'rafael', '2022-09-07 15:29:00', '2022-09-07 15:45:00', 'algo', 'quebrou', 'algo aconteceu', 'algo foi consertado', '00:00:00', '', 1),
-(18, 'joana', 'Administrativo', '', '2022-09-07 15:00:00', 'C. emergencial', 'rafael', '2022-09-07 15:31:00', '2022-09-07 16:00:00', 'algo', 'quebrou', 'algo aconteceu', 'algo foi consertado', '00:00:00', '', 1),
-(19, 'Joao Santos', 'Produção', '12-Terco 1', '2022-09-07 15:04:00', 'C. emergencial', 'cleber', '2022-09-07 20:05:00', '2022-09-08 18:20:00', 'algo', 'quebrou', 'algo quebrou', 'alguma coisa', '00:00:00', '', 1),
-(20, 'joao santos', 'Administrativo', '', '2022-09-07 15:05:00', 'C. emergencial', 'cleber', '2022-09-07 16:05:00', '2022-09-07 17:05:00', 'algo', 'quebrou', 'algo aconteceu', 'algo foi consertado', '00:00:00', '', 1),
-(21, 'joao santos', 'Administrativo', '', '2022-09-07 15:05:00', 'C. emergencial', 'rafael', '2022-09-07 20:10:00', '2022-09-07 20:50:00', 'algo', 'quebrou', 'algo aconteceu', 'algo foi consertado', '00:00:00', '', 1),
-(22, 'joao santos', 'Administrativo', '', '2022-09-07 15:06:00', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', '', 'mais alguma coisa', '', '00:00:00', NULL, 0),
-(23, 'joao', 'Administrativo', '', '2022-09-07 20:50:00', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', '', 'algo aconteceu', '', '00:00:00', NULL, 0),
-(24, 'Joao Santos', 'Produção', '12-Terco 1', '2022-09-07 20:51:00', 'C. emergencial', 'rafael', '2022-09-07 20:50:00', '2022-09-07 21:00:00', 'chacoalhadeira', 'emenda solta', 'algo esta vazando', 'apertado emenda', '00:00:00', '', 1),
-(26, 'pedro', 'Expedição', '', '2022-09-11 10:31:00', 'C. emergencial', 'rafael', '2022-09-11 10:35:00', '2022-09-11 10:50:00', 'rotuladeria', 'quebrou', 'O caminhão quebrou', 'algo foi feito', '00:00:00', '', 1);
+INSERT INTO `ordemservico` (`id`, `nomeRequisitante`, `setor`, `linhaProducao`, `dataRequisicao`, `descricaoRequisicao`, `concluido`, `id_relatorioTecnico`) VALUES
+(28, 'João', 'Formulação', '', '2022-10-26 16:52:00', 'Algo ocorreu', 1, NULL),
+(30, 'Cleber', 'Produção', 'Linha 5 (LPB)', '2022-10-26 19:07:00', 'Algo quebrou', 1, 22),
+(31, 'Pedro', 'Produção', 'Linha 5 (LPB)', '2022-10-26 19:48:00', 'Maquina não liga ', 1, 23);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `relatoriotecnico`
+--
+
+CREATE TABLE `relatoriotecnico` (
+  `id` int(11) NOT NULL,
+  `id_requisicao` int(11) NOT NULL,
+  `tipoManutencao` varchar(30) NOT NULL,
+  `nomeManutencao` text NOT NULL,
+  `dataInicio` datetime NOT NULL,
+  `dataTermino` datetime NOT NULL,
+  `parteProblema` varchar(100) NOT NULL,
+  `motivoProblema` text NOT NULL,
+  `solucao` text NOT NULL,
+  `tempoParada` time DEFAULT NULL,
+  `nomeTerceiros` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `relatoriotecnico`
+--
+
+INSERT INTO `relatoriotecnico` (`id`, `id_requisicao`, `tipoManutencao`, `nomeManutencao`, `dataInicio`, `dataTermino`, `parteProblema`, `motivoProblema`, `solucao`, `tempoParada`, `nomeTerceiros`) VALUES
+(1, 28, 'C. emergencial', 'João', '2022-10-26 00:05:00', '2022-10-26 20:05:00', 'Maquina', 'Vazando', 'Vazamento consertado', '00:00:00', ''),
+(2, 28, 'C. emergencial', 'João', '2022-10-26 00:00:00', '2022-10-26 00:00:00', 'Maquina', 'Vazando', 'Apertado cano', '00:00:00', ''),
+(3, 28, 'C. emergencial', 'João', '2022-10-26 00:00:00', '2022-10-26 00:00:00', 'Maquina', 'Vazamento', 'Apertado cano', '00:00:00', ''),
+(22, 30, 'C. programada', 'Rafael', '2022-10-26 19:10:00', '2022-10-26 20:10:00', 'maquina', 'Qebrada', 'consertada', '00:00:00', ''),
+(23, 31, 'C. emergencial', 'Rafael', '2022-10-26 19:50:00', '2022-10-26 20:10:00', 'Maquina', 'Quebrado', 'consertado', '00:00:00', '');
 
 -- --------------------------------------------------------
 
@@ -100,6 +109,12 @@ ALTER TABLE `ordemservico`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `relatoriotecnico`
+--
+ALTER TABLE `relatoriotecnico`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices para tabela `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -113,7 +128,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `ordemservico`
 --
 ALTER TABLE `ordemservico`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT de tabela `relatoriotecnico`
+--
+ALTER TABLE `relatoriotecnico`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
